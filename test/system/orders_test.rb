fdfab24d6c0b0adc56b31ea1a8c937aed5b9f10a
nathan <nathans@scribd.com>
2020-11-13 08:21:05 -0500

    configured test & prod environments to enable sending of outbound emails

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,10 +1,15 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   test 'check routing number' do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
-    first('.catalog li').check_on 'Add to Cart'
+    first('.catalog li').click_on 'Add to Cart'
 
     click_on 'Checkout'
 
@@ -12,10 +17,32 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_address', with: '123 Main Street'
     fill_in 'order_email', with: 'dave@example.com'
 
-    assert_no_selector "#order_routin_number"
+    assert_no_selector '#order_routin_number'
 
     select 'Check', from: 'pay_type'
 
-    assert_selector "#order_routing_number"
+    assert_selector '#order_routing_number'
+    fill_in 'Routing #', with: '123456'
+    fill_in 'Account #', with: '987654'
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal 'Dave Thomas',      order.name
+    assert_equal '123 Main Street',  order.address
+    assert_equal 'dave@example.com', order.email
+    assert_equal 'Check',            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['dave@example.com'],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
   end
 end
